5131a2c0f0c06e06afd5c873a99a0218d1399dc0
joaofurini <jp.furini@hotmail.com>
2020-08-19 21:48:23 +0800

    Create mobile header

---
+++ src/components/MobileHeader/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, ProfileCircle, SearchInput, MessageIcon } from './styles';
+
+const MobileHeader: React.FC = () => {
+  return (
+
+    <Container>
+      <ProfileCircle src="" />
+      <SearchInput placeholder="Pesquisar" />
+      <MessageIcon />
+    </Container>
+
+  );
+}
+
+export default MobileHeader;
